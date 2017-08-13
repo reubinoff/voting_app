b878ef87e33f1c7192a2dcc8598b6195a624abff
Moshe Reubinoff <Moshe.Reubinoff@harmonicinc.com>
2017-08-13 16:01:01 +0300

    Adding unit testing and router

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ var morgan = require('morgan')
 require('dotenv').config()
 
 var DB_init = require('./db/config');
+const connection_string = DB_init()
+
+
+const routers = require('./routers')
 
 
 const port = process.env.PORT
 const app_name = process.env.APP_NAME
-const connection_string = DB_init()
+
 
 logger.add(logger.transports.File, { filename: "/tmp/" + app_name + ".log" })
 
@@ -25,7 +29,7 @@ const pages_path = __dirname + "/www"
 app.set('view engine', 'pug')
 app.set('views', pages_path)
 app.set('view options', {
-  layout: false
+    layout: false
 });
 
 logger.log(express.static(pages_path))
@@ -38,23 +42,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //router
 
-// app.post('/file', upload.single('avatar'), function (req, res, next) {
-  
-  
-// })
+app.use('/api/polls',routers.polls_router.get_router())
 
-// index page
+
+
+// index test page
 app.get('/', function (req, res) {
-  res.render('index', {
-    title: 'my main page'
-  })
+    res.render('index', {
+        title: 'my main page'
+    })
 })
 
 
 
 // Server startup
 app.listen(port, function () {
-  logger.info(app_name + ' listening on port ', port)
+    logger.info(app_name + ' listening on port ', port)
 })
 
+module.exports = app;
 
